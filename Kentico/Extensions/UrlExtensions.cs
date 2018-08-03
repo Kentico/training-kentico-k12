5cad6cf0f22d058c72845ea7e9530e18a50f788c
Enngage <ennspassion@gmail.com>
2018-08-03 10:38:09 -0400

    Adds wrapper around Kentico ImageUrl metod

---
+++ Kentico/Extensions/UrlExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Kentico.Content.Web.Mvc;
+using Kentico.Web.Mvc;
+using System.Web.Mvc;
+using Kentico.Models;
+
+namespace Kentico.Extensions
+{
+    public static class UrlExtensions
+    {
+        public static string KenticoImageUrl(this UrlHelper target, string path, IImageSizeConstraint size = null)
+        {
+            return target.Kentico().ImageUrl(path, size?.GetSizeConstraint() ?? SizeConstraint.Empty);
+        }
+
+    }
+}
