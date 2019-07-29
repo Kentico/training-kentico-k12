7ebd6f723cb71ada7063cea5df8700b9da88432c
Jan Lenoch <jan.lenoch@hotmail.cz>
2019-07-29 14:51:03 +0200

    Sync culture retrieval across branches.

--- Business/Dto/Culture/CultureDto.cs
+++ Business/Dto/Culture/CultureDto.cs
@@ -1,9 +1,18 @@
-﻿namespace Business.Dto.Culture
+﻿using System;
+
+namespace Business.Dto.Culture
 {
+    /// <summary>
+    /// A lightweight representation of Kentico's <see cref="CMS.SiteProvider.CultureSiteInfo"/> objects.
+    /// </summary>
     public class CultureDto : IDto
     {
+        public Guid CultureGuid { get; set; }
+
         public string CultureCode { get; set; }
+
         public string CultureName { get; set; }
+
         public string CultureShortName { get; set; }
     }
 }
--- Business/Services/Culture/CultureService.cs
+++ Business/Services/Culture/CultureService.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+
+using CMS.SiteProvider;
+
 using Business.Dto.Culture;
 using Business.Services.Context;
-using CMS.SiteProvider;
 
 namespace Business.Services.Culture
 {
@@ -15,16 +17,17 @@ namespace Business.Services.Culture
             SiteContextService = siteContextService;
         }
 
-        public IEnumerable<CultureDto> GetSiteCultures()
-        {
-            return CultureSiteInfoProvider.GetSiteCultures(SiteContextService.SiteName).Items.Select(m =>
-                new CultureDto()
-                {
-                    CultureCode = m.CultureCode,
-                    CultureName = m.CultureName,
-                    CultureShortName = m.CultureShortName
-                }
+        public IEnumerable<CultureDto> GetSiteCultures() =>
+            CultureSiteInfoProvider.GetSiteCultures(SiteContextService.SiteName)
+                .Items
+                .Select(culture =>
+                    new CultureDto()
+                    {
+                        CultureGuid = culture.CultureGUID,
+                        CultureCode = culture.CultureCode,
+                        CultureName = culture.CultureName,
+                        CultureShortName = culture.CultureShortName
+                    }
             );
-        }
     }
 }
