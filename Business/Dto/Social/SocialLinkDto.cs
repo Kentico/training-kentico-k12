00d189d8c901ace44d90547906a846298b230896
Jan Lenoch <jan.lenoch@hotmail.cz>
2019-07-29 17:23:31 +0200

    Fixes #41

--- Business/Dto/Social/SocialLinkDto.cs
+++ Business/Dto/Social/SocialLinkDto.cs
@@ -2,7 +2,7 @@
 
 namespace Business.Dto.Social
 {
-    public class SocialLinkDto
+    public class SocialLinkDto : IDto
     {
         public string Title { get; set; }
         public string Url { get; set; }
